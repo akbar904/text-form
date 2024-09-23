b7e2df65981acffb846e66351c04d7c036c050b6
zharfan-a <zharfan@walturn.com>
2024-09-23 16:17:55 +0700

    Update 15 files

--- src/lib/cubits/login_cubit.dart
+++ src/lib/cubits/login_cubit.dart
@@ -3,20 +3,16 @@ import 'package:bloc/bloc.dart';
 
 class LoginState {
 	final String email;
-	final String password;
 
 	LoginState({
 		this.email = '',
-		this.password = '',
 	});
 
 	LoginState copyWith({
 		String? email,
-		String? password,
 	}) {
 		return LoginState(
 			email: email ?? this.email,
-			password: password ?? this.password,
 		);
 	}
 }
@@ -26,9 +22,5 @@ class LoginCubit extends Cubit<LoginState> {
 
 	void updateEmail(String email) {
 		emit(state.copyWith(email: email));
-	}
-
-	void updatePassword(String password) {
-		emit(state.copyWith(password: password));
 	}
 }
--- src/lib/models/user_model.dart
+++ src/lib/models/user_model.dart
@@ -1,21 +1,18 @@
 
 class User {
 	final String email;
-	final String password;
 
-	User({required this.email, required this.password});
+	User({required this.email});
 
 	Map<String, dynamic> toJson() {
 		return {
 			'email': email,
-			'password': password,
 		};
 	}
 
 	factory User.fromJson(Map<String, dynamic> json) {
 		return User(
 			email: json['email'],
-			password: json['password'],
 		);
 	}
 }
